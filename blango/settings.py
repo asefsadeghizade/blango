eb8647c55aa8c4b89e1de148e149abbd8d76819f
Asef Zade <asefsadeghizade@hotmail.com>
2024-09-23 19:57:13 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -50,6 +50,7 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites", 
         'django.contrib.staticfiles',
 
         "blango_auth",
@@ -60,6 +61,10 @@ class Dev(Configuration):
         "debug_toolbar",
 
         "blog",
+
+        "allauth", "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google",
     ]
 
     #CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
@@ -225,6 +230,13 @@ class Dev(Configuration):
     ACCOUNT_ACTIVATION_DAYS = 7
 
     # REGISTRATION_OPEN = False
+
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
 
 class Prod(Dev):
