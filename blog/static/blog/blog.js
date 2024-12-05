5ec745f4854f725876811845e0cea257c13fab34
Asef Zade <asefsadeghizade@hotmail.com>
2024-12-05 12:05:39 +0000

    Finish JavaScript intro and fundamentals

---
+++ blog/static/blog/blog.js
@@ -0,0 +1,6 @@
+alert("Hello, world!")
+
+
+const framework = "django"
+const language = "Python"
+alert(framework + " is written in " + language)
--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path("", views.index),
   path("post/<slug>/", views.post_detail, name="blog-post-detail"),
   path("ip/", views.get_ip, name="get-ip"),
+  path("post-table/", views.post_table, name="blog-post-table"),
 ]
